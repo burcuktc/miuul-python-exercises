b20702dd03a3b6f49c9ae55c6625addfeafc1ea1
burcuktc <105872598+burcuktc@users.noreply.github.com>
2023-08-09 15:11:32 +0300

    Add files via upload

---
+++ exercises.py
@@ -0,0 +1,154 @@
+#for, enumerate, zip,lambda, comprehensions alıştırmaları
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    print(salary)
+
+#maaşlara %20 zam:
+for salary in salaries:
+    print(int(salary*20/100 + salary))
+
+#%30 zam
+for salary in salaries:
+    print(int(salary*30/100 + salary))
+
+
+def new_salary(salary, rate):
+    return int(salary*rate/100 + salary)
+
+new_salary(1500, 10)
+new_salary(2000, 20)
+
+for salary in salaries:
+    print(new_salary(salary, 20))
+
+
+salaries2 = [10700, 25000, 30400, 40300, 50200]
+
+for salary in salaries2:
+    print(new_salary(salary, 15))
+
+for salary in salaries:
+    if salary >= 3000:
+        print(new_salary(salary, 10))
+    else:
+        print(new_salary(salary, 20))
+
+# Enumerate: Otomatik Counter/Indexer ile for loop
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+for student in students:
+    print(student)
+
+for index, student in enumerate(students):
+    print(index, student)
+
+A = []
+B = []
+
+for index, student in enumerate(students):
+    if index % 2 == 0:
+        A.append(student)
+    else:
+        B.append(student)
+
+
+# Uygulama
+# divide_students fonksiyonu yazınız.
+# Çift indexte yer alan öğrencileri bir listeye alınız.
+# Tek indexte yer alan öğrencileri başka bir listeye alınız.
+# Fakat bu iki liste tek bir liste olarak return olsun.
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+
+def divide_students(students):
+    groups = [[], []]
+    for index, student in enumerate(students):
+        if index % 2 == 0:
+            groups[0].append(student)
+        else:
+            groups[1].append(student)
+    print(groups)
+    return groups
+st = divide_students(students)
+st[0]
+st[1]
+
+# Zip
+#######################
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+departments = ["mathematics", "statistics", "physics", "astronomy"]
+
+ages = [23, 30, 26, 22]
+
+list(zip(students, departments, ages))
+
+# lambda,
+def summer(a, b):
+    return a + b
+
+summer(1, 3) * 9
+
+new_sum = lambda a, b: a + b
+
+new_sum(4, 5)
+
+# COMPREHENSIONS
+
+# List Comprehension
+#######################
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+def new_salary(x):
+    return x * 20 / 100 + x
+
+for salary in salaries:
+    print(new_salary(salary))
+
+null_list = []
+
+for salary in salaries:
+    null_list.append(new_salary(salary))
+
+null_list = []
+
+for salary in salaries:
+    if salary > 3000:
+        null_list.append(new_salary(salary))
+    else:
+        null_list.append(new_salary(salary * 2))
+
+[new_salary(salary * 2) if salary < 3000 else new_salary(salary) for salary in salaries]
+[salary * 2 for salary in salaries]
+[salary * 2 for salary in salaries if salary < 3000]
+[salary * 2 if salary < 3000 else salary * 0 for salary in salaries]
+[new_salary(salary * 2) if salary < 3000 else new_salary(salary * 0.2) for salary in salaries]
+students = ["John", "Mark", "Venessa", "Mariam"]
+students_no = ["John", "Venessa"]
+
+
+[student.lower() if student in students_no else student.upper() for student in students]
+[student.upper() if student not in students_no else student.lower() for student in students]
+
+
+# Dict Comprehension
+
+dictionary = {'a': 1,
+              'b': 2,
+              'c': 3,
+              'd': 4}
+
+dictionary.keys()
+dictionary.values()
+dictionary.items()
+
+{k: v ** 2 for (k, v) in dictionary.items()}
+
+{k.upper(): v for (k, v) in dictionary.items()}
+
+{k.upper(): v*2 for (k, v) in dictionary.items()}
